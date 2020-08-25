2447a4f6e3f9291d80fbb7c76fa08f18ac973779
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-08-25 19:56:26 -0400

    Stripped down base code

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoPrefixer = require('autoprefixer');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
   entry: {
-    'main': './src/index.js',
-    'content': './src/content.js',
+    main: './src/index.js',
   },
   output: {
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
   },
-  devServer: {
-    contentBase: './dist',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins() {
+                return [autoPrefixer];
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader',
+        ],
+      },
+    ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      meta: {
+        'theme-color': '#4285f4',
+      },
+      chunks: ['main'],
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
